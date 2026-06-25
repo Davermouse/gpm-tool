cfe009392341c608300f6a0be37e42c9729022c1
David Miles <quantumdave@Gmail.com>
2026-06-25 20:58:26 +0100

    Add GH Action deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    build: {
-      outDir: 'build',
-    },
+    base: "/gpm-tool/",
     plugins: [react()],
   };
 });
